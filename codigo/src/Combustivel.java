3adebd79f7665e25a849b78645ba50d8586ca41d
Leonardo Augusto Pereira do Carmo <78862366+Leozin11@users.noreply.github.com>
2022-11-10 12:18:11 -0300

    Create Combustivel.java

---
+++ codigo/src/Combustivel.java
@@ -0,0 +1,21 @@
+package codigo.src;
+
+public class Combustivel {
+
+	double gasolina = 4.8;
+	double etanol = 3.65;
+	double diesel = 6.65;
+
+
+
+	void calcularConsumoMedio() {
+
+	}
+
+	void calcularRota() {
+		
+	}
+	
+	
+	
+}
